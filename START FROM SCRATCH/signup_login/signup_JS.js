18325e9390b9d6d1403ed8761e2e7826d4efd794
GhilliedFox <richardcfenton@gmail.com>
2022-03-10 19:17:45 -0800

    added signup items

--- START FROM SCRATCH/signup_login/signup_JS.js
+++ START FROM SCRATCH/signup_login/signup_JS.js
@@ -5,11 +5,11 @@ const secondForm = document.getElementById("form2");
 const container = document.querySelector(".container");
 
 signInBtn.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
+  container.classList.remove("right-panel-active");
 });
 
 signUpBtn.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
+  container.classList.add("right-panel-active");
 });
 
 firstForm.addEventListener("submit", (e) => e.preventDefault());
