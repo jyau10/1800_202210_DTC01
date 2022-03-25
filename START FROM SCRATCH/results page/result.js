7cfc9afa6a0d189296167d260419fb00e91bcd12
GhilliedFox <richardcfenton@gmail.com>
2022-03-24 22:29:45 -0800

    Attempt to fix firestore

--- START FROM SCRATCH/results page/result.js
+++ START FROM SCRATCH/results page/result.js
@@ -1,14 +1,14 @@
 function populateCardsDynamically() {
   let athleteCardTemplate = document.getElementById("athleteCardTemplate");
-  let athleteCardGroup = document.getElementById("athleteCardGroup");
 
   db.collection("athletes")
+    .limit(20)
     .get()
     .then((allAthletes) => {
       allAthletes.forEach((doc) => {
-        var athleteName = doc.data().name; //gets the name field
-        var athleteID = doc.data().id; //gets the unique ID field
-        var athleteLength = doc.data().length; //gets the length field
+        var athleteName = doc.data().GivenName; //gets the name field
+        var athleteID = doc.data().code; //gets the unique ID field
+        var athleteFamilyName = doc.data().FamilyName; //gets the length field
         let testathleteCard = athleteCardTemplate.content.cloneNode(true);
         testathleteCard.querySelector(".card-title").innerHTML = GivenName;
         testathleteCard.querySelector(".card-length").innerHTML = name;
@@ -23,6 +23,6 @@ populateCardsDynamically();
 
 insertName();
 
-function setAthleteData(id) {
-  localStorage.setItem("athleteID", id);
+function setAthleteData(code) {
+  localStorage.setItem("athleteID", code);
 }
